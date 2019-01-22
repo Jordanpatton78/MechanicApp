c4f9e5a331528bca68a1cd5de63abb430c081746
Jordanpatton78 <44303813+Jordanpatton78@users.noreply.github.com>
2019-01-21 17:11:41 -0700

    Update MainActivity.java

--- app/java/MainActivity.java
+++ app/java/MainActivity.java
@@ -11,25 +11,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button transButton1;
-        Button transButton2;
+        Button transButton1=findViewById(R.id.transButton1);
+        Button transButton2=findViewById(R.id.transButton2);
+        Button transButton3=findViewById(R.id.transButton3);
+        Button transButton4=findViewById(R.id.transButton4);
         final TextView transText1 = findViewById(R.id.transText1);
         final TextView transText2=findViewById(R.id.transText2);
+        final TextView transText3=findViewById(R.id.transText3);
+        final TextView transText4=findViewById(R.id.transText4);
         final String transResponse=getString(R.string.transResponse);
-        transButton1= findViewById(R.id.transButton1);
-        transButton2=findViewById(R.id.transButton2);
 
         transButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            transText1.setText(transResponse);
+            transText1.setText(transResponse+" You probably have low transmission fluid, or your shift cables or clutch linkage need adjusting.");
             }
         });
         transButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                transText2.setText(transResponse);
+                transText2.setText(transResponse+" Your transmission fluid is low, leaking, or dirty.");
             }
                                         });
+        transButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transText3.setText(transResponse+" You have low or dirty transmission fluid, or your transmission is worn down.");
+            }
+        });
+        transButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transText4.setText(transResponse+" Hearing something that doesn't sound normal means your transmission needs to be checked.");
+            }
+        });
+
+
     }
 }
